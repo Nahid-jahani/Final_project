159bf1084a457e3ce2d238b8d047606ace4909da
Nahid <jahaninahide@gmailcom>
2021-03-24 22:53:06 -0700

    Last update

--- script/Dice.js
+++ script/Dice.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 // Selecting elements
 
@@ -7,6 +7,8 @@ const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
+
+
 const diceEl_1 = document.querySelector('.diceMe');
 const diceEl_2 = document.querySelector('.diceMe2');
 const diceEl_3 = document.querySelector('.diceCom');
@@ -14,13 +16,16 @@ const diceEl_4 = document.querySelector('.diceCom2');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 
+const $show = $('.hide');
+const $content = $('.show');
 const popup = document.getElementById("pop-up");
 const btnClose = document.getElementById("btn-close");
 const winerGame = document.querySelector('#winer');
 
-let tatal = 0;
-let tatal2 = 0;
+let tatalofPlayer = 0;
+let tatalOfComputer = 0;
 let rollCount = 0;
+
 
 
 btnClose.addEventListener('click', function () {
@@ -29,17 +34,19 @@ btnClose.addEventListener('click', function () {
 })
 
 
+
+
 // Starting conditions
 const init = () => {
 
-    tatal = 0;
-    tatal2 = 0;
-    rollCount = 0;
+    tatalofPlayer = 0;
+    tatalOfComputer = 0;
+    rollCount = 1;
     score0El.textContent = 0;
     score1El.textContent = 0;
     current0El.textContent = 0;
     current1El.textContent = 0;
-    //diceEl_1.html = `<p>🎲</p>`;
+
     diceEl_1.src = "img/dice.jpg";
     diceEl_2.src = "img/dice.jpg";
     diceEl_3.src = "img/dice.jpg";
@@ -73,21 +80,21 @@ const dice = () => {
 
 
 btnRoll.addEventListener('click', function () {
-    if (rollCount < 3) {
+    if (rollCount <= 3) {
         rollCount++
         console.log(rollCount);
-        // 1. Generating a random dice roll
 
-        let b, c, d, e;
+
+        let dic1, dic2, dic3, dic4;
         // 2. Display dices
 
-        diceEl_1.src = `img/dice-${b = dice()}.png`;
+        diceEl_1.src = `img/dice-${dic1 = dice()}.png`;
 
-        diceEl_2.src = `img/dice-${c = dice()}.png`;
+        diceEl_2.src = `img/dice-${dic2 = dice()}.png`;
 
-        diceEl_3.src = `img/dice-${d = dice()}.png`;
+        diceEl_3.src = `img/dice-${dic3 = dice()}.png`;
 
-        diceEl_4.src = `img/dice-${e = dice()}.png`;
+        diceEl_4.src = `img/dice-${dic4 = dice()}.png`;
 
         // chackForOne(b, c);
         // chackSame(b, c);
@@ -95,51 +102,48 @@ btnRoll.addEventListener('click', function () {
         // chackSame(d, e);
 
 
-
-        // 3. Check for rolled 1
         // 3. Check for rolled 1
 
-        if (b == 1 || c == 1) {
-            c = 0;
-            b = 0;
+        if (dic1 == 1 || dic2 == 1) {
+            dic2 = 0;
+            dic1 = 0;
         }
 
-        if (d == 1 || e == 1) {
-            d = 0;
-            e = 0;
+        if (dic3 == 1 || dic4 == 1) {
+            dic3 = 0;
+            dic4 = 0;
         }
 
-
-
-        if (b == c) {
-            b = (b + b) * 2;
-            c = 0;
-        }
-        if (d == e) {
-            d = (d + e) * 2;
-            e = 0;
+        if (dic1 == dic2) {
+            dic1 = (dic1 + dic1) * 2;
+            dic2 = 0;
         }
 
-        score0El.textContent = `${b + c}`;
-        score1El.textContent = `${d + e}`;
+        if (dic3 == dic4) {
+            dic3 = (dic3 + dic4) * 2;
+            dic4 = 0;
+        }
+
+        current0El.textContent = `${dic1 + dic2}`;
+        current1El.textContent = `${dic3 + dic4}`;
 
 
-        tatal += b + c;
-        tatal2 += d + e;
+        tatalofPlayer += dic1 + dic2;
+        tatalOfComputer += dic3 + dic4;
 
-        current0El.textContent = tatal;
-        current1El.textContent = tatal2;
+        score0El.textContent = tatalofPlayer;
+        score1El.textContent = tatalOfComputer;
 
     } else {
-        if (tatal > tatal2) {
+        if (tatalofPlayer > tatalOfComputer) {
 
-            winerGame.textContent = `  Player  is WON. * * * Congratulations * * *  .
-            Totale scores are : ${tatal} `
+            winerGame.innerHTML = ` Player  is WON.&#129351;  Congratulation. <br/>
+        Totale scores are: ${tatalofPlayer} `
             popUp()
 
         } else {
-            winerGame.textContent = `  Computer is WON.  * * * Congratulations * * *  .
-            Totale scores are : ${tatal2} `
+            winerGame.innerHTML = `  Computer is WON.&#129351; Congratulation.<br/>
+        Totale scores are: ${tatalOfComputer} `
             popUp()
         }
 
@@ -147,10 +151,32 @@ btnRoll.addEventListener('click', function () {
 });
 
 const popUp = () => {
-    popup.style.pointerEvents = "all";
+    //   popup.style.pointerEvents = "all";
     popup.style.opacity = "9";
+
+    popup.style.zIndex = '1000';
+    init();
+
 }
 
+//Show the Information about Games
+$content.hide();
+
+// Accordion
+$show.click(function () {
+    /*
+    if the user is clicking the tab for 
+    content that is alraedy visible, slide it up
+    */
+    if ($(this).next().is(':visible')) {
+        $(this).next().slideUp();
+    } else {
+        //otherwise, close all content
+        $content.slideUp();
+        //and open the chosen content
+        $(this).next().slideDown();
+    }
+});
 
 
 btnNew.addEventListener('click', init);
